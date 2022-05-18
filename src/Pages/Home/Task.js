402bfe7b96d63edd7e3ff37b6505ca13b53ef82e
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-19 01:15:59 +0600

    task item delete mechanism implimented

--- src/Pages/Home/Task.js
+++ src/Pages/Home/Task.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 const Task = (props) => {
   const { _id, title, description, email, status } = props.task
+
+  const deleteBtnClickHandler = (id) => {
+    props.deleteTask(id)
+  }
+
   return (
     <div className='bg-gray-200 p-4 rounded-lg'>
       <h1 className='font-semibold'>{title}</h1>
@@ -10,7 +15,14 @@ const Task = (props) => {
       </p>
       <div className='flex items-center justify-center gap-x-4 mt-4'>
         <button className='btn btn-outline btn-success'>Done</button>
-        <button className='btn btn-outline btn-error'>Delete</button>
+        <button
+          onClick={() => {
+            deleteBtnClickHandler(_id)
+          }}
+          className='btn btn-outline btn-error'
+        >
+          Delete
+        </button>
       </div>
     </div>
   )
--- src/Pages/Home/TaskList.js
+++ src/Pages/Home/TaskList.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { toast } from 'react-toastify'
 import auth from '../../firebase.init'
 import Task from './Task'
 
 const TaskList = ({ taskId }) => {
   const [user] = useAuthState(auth)
   const [taskList, setTaskList] = useState([])
+
+  const deleteTask = (id) => {
+    fetch(`http://localhost:5000/task/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          const filteredTaskList = taskList.filter((task) => task._id !== id)
+          setTaskList(filteredTaskList)
+          toast('Task is deleted')
+        }
+      })
+  }
 
   useEffect(() => {
     fetch(`http://localhost:5000/task?email=${user.email}`)
@@ -18,7 +33,7 @@ const TaskList = ({ taskId }) => {
       <div className='card-body'>
         <h2 className='text-2xl font-bold text-center'>Task List</h2>
         {taskList.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task key={task._id} task={task} deleteTask={deleteTask} />
         ))}
       </div>
     </div>
