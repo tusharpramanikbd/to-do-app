aa293a1a7f5e6bf3c19e675380a773f7c3438bef
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-18 23:47:16 +0600

    require auth implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import RequireAuth from './Pages/Login/RequireAuth'
 import Navbar from './Pages/Shared/Navbar'
 
 function App() {
@@ -9,8 +10,22 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
+        <Route
+          path='/'
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/home'
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
